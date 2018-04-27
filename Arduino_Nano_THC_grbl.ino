30b8374d6bb236c5f9f860abab2ec90877b16a75
MoRehberg <rehberg.moritz@gmail.com>
2018-04-27 17:34:17 +0200

    ausprogn 2

--- Arduino_Nano_THC_grbl.ino
+++ Arduino_Nano_THC_grbl.ino
@@ -23,22 +23,22 @@ Variablen:
 
 	
 
-	O:	PauseProgramm
-
-		ArcEnable
-
-		zDir
-
-		zPuls
+	O: 	int PauseProgramm	= #PINNUMMER
+		int ArcEnable       = #PINNUMMER
+		int zDir            = #PINNUMMER
+		int zPuls           = #PINNUMMER
 
 		
 
 	_Var:
 
-		countSteps
+		int countSteps = 0;
 
-		iStep
+		int iStep = 0;
 
+		int iVelo = 0;	//mm/min
+		
+		int iDistRelease = 0;	//mm
 		
 
 		TIMER_PROGRAMMIEREN!!!
@@ -54,7 +54,7 @@ Variablen:
 /*
 Funktionen:
 
-				moveUp();
+				moveUp();	// per call 1 Step
 
 				moveDown();
 
@@ -78,7 +78,7 @@ THCDown			 = NOT digitalRead(Pinx);
 
 THCArc			 = NOT digitalRead(Pinx);
 
-SpindleEnable	 = NOT digitalRead(Pinx);
+SpindleEnable	         = NOT digitalRead(Pinx);
 
 
 
@@ -108,46 +108,113 @@ ELSE {
 
 	
 
-	IF 
+	IF	SpindleEnable && NOT iStep {
 
+		iStep = 1; 		// initial, wenn schrittkette auf 0
+		
+	}
+	
+	Elsif NOT SpindleEnable {
+		iStep = 0;
+	}
 
+switch(iStep){
 
 //Startsignal dann Schrittkette start, sonst disable move, Plasma aus
 
-
+case 0:		//Alle ausgänge aus
 
 //Programm pause
 
+case 1:		digitalWrite(PauseProgram, HIGH);
 
+		iStep = 2;
 
 //Antasten
 
+case 2:		while(NOT LimitDown){
+	
+		moveDown(iVelo);
+		}	
 
-
+		iStep = 3;
+		
 //Freifahren
+		
+case 3:		iDistRelease
+		
+		for(...)	{
+		
+			moveUp();
+			
+			iCountStepZ ++;
+			
+		}
 
-
-
+		iStep = 4;
+		
 //Start Lichtbogen
 
+case 4:		digitalWrite(ArcEnable, HIGH);
 
-
+		iStep = 5;
+		
 //Einstechen
 
-
+case 5:		
 
 //End Programm Pause
 
+case 6:        digitalWrite(PauseProgram, LOW);
 
+               iStep = 7;
 
 //LOOP moveUp(),moveDown()
 
+case 7:        if
+
+}
+
+}
+
+
+//CounterStepMaster
+
+
+
+int moveDown(int PinDir, int PinStep){
+  boolean Dir = 0;
+  boolean Step;
+  
+  
+  
+}
 
 
 
 
-
-
-
+boolean TON(int TimeValueMS, boolean IN){
+ 
+  boolean OUT;
+  
+if (IN){
+  newTime = millis();
+    
+    if (newTime - oldTime >= TimeValueMS){
+      
+     return 1;
+      
+    }
+          
+}
+ 
+else {
+ 
+  int oldTime = millis();
+  int newTime = millis();
+  return 0;
+} 
+  
+}
 
 
